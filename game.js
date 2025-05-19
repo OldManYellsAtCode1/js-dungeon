fa39964347e447e8c61e81582b428f139970f28f
Matthew Erickson <matt.eeee@gmail.com>
2025-05-20 00:40:32 +1000

    Add keyboard controls for the hero.

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
 let knightX = 0;
+let knightY = 0;
+
 const knightImg = new Image();
 knightImg.src = './assets/knight-sheet.png';
 
@@ -11,9 +13,23 @@ function draw() {
     ctx.fillStyle = 'deepSkyBlue';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.drawImage(knightImg, 0, 0, 15, 19, knightX, 0, 15, 19);
+    ctx.drawImage(knightImg, 0, 0, 15, 19, knightX, knightY, 15, 19);
 
-    knightX += 0.5;
+    if (keys.get('ArrowRight')) {
+        knightX += 0.5;
+    }
+
+    if (keys.get('ArrowLeft')) {
+        knightX -= 0.5;
+    }
+
+    if (keys.get('ArrowUp')) {
+        knightY -= 0.5;
+    }
+
+    if (keys.get('ArrowDown')) {
+        knightY += 0.5;
+    }
 }
 
 requestAnimationFrame(draw);
